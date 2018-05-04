004ab82be339d75653f96a08c704de612bfb3495
yuri <urimir@gmail.com>
2018-05-05 00:49:38 +0300

    * Getting video's info

--- src/youtube_scrapper.py
+++ src/youtube_scrapper.py
@@ -16,15 +16,14 @@ def _get_youtube_video_info(url):
         return ydl.extract_info(url)
 
 
-def get_youtube_video_info(url):
+def get_youtube_video_data(url):
     info = _get_youtube_video_info(url)
-    print('Url', 'url')
-    print('Title', info['title'])
-    print('Upload date', info['upload_date'])
-    print('Average Rating', info['average_rating'])
+    print('Url:', url)
+    print('Title:', info['title'])
+    print('Upload date:', info['upload_date'])
+    print('Average Rating:', info['average_rating'])
 
 
 if __name__ == '__main__':
     url = 'http://www.youtube.com/watch?v=BaW_jenozKc'
-    get_youtube_video_info(url)
-
+    get_youtube_video_data(url)
